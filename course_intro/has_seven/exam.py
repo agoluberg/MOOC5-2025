56bf3dfbdfccc78b53cc512df0f1e38fa1a68517
Ago Luberg <ago.luberg@ttu.ee>
2025-01-27 10:18:27 +0200

    Create exam.py

---
+++ course_intro/has_seven/exam.py
@@ -0,0 +1,16 @@
+"""Has seven."""
+
+
+def has_seven(nums: list) -> bool:
+    """
+    Whether the list has three 7s and no repeated consecutive elements.
+
+    Given a list if ints, return True if the value 7 appears in the list exactly 3 times
+    and no consecutive elements have the same value.
+
+    has_seven([1, 2, 3]) => False
+    has_seven([7, 1, 7, 7]) => False
+    has_seven([7, 1, 7, 1, 7]) => True
+    has_seven([7, 1, 7, 1, 1, 7]) => False
+    """
+    pass
